b049173d93d682296c20076fedfd9864e8f1c3ae
Sithag <43814445+Sithag@users.noreply.github.com>
2019-07-12 00:58:06 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,144 @@
+var count = 0;
+document.getElementById('btn').addEventListener("click", function colorred() {
+  count = count + 1
+  console.log(count);
+
+  if (count == 1) {
+    document.getElementById('text').innerHTML = "AÏE !";
+  }
+
+  if (count == 2) {
+    document.getElementById('text').innerHTML = "Pas si fort..";
+  }
+
+  if (count == 3) {
+    document.getElementById('text').innerHTML = "J'imagine que vous avez compris le principe";
+  }
+
+  if (count == 4) {
+    document.getElementById('text').innerHTML = "Je m'apelle Simon Destanque";
+  }
+
+  if (count == 5) {
+    document.getElementById('text').innerHTML = "J'ai 19 ans";
+  }
+
+  if (count == 6) {
+    document.getElementById('text').innerHTML = "Et je vais rentrer en 3ème année à l'ESD Paris pour l'année 2019/2020";
+  }
+
+  if (count == 7) {
+    document.getElementById('text').innerHTML = "Pour cette année je cherche un contrat de professionnalisation";
+  }
+
+  if (count == 8) {
+    document.getElementById('text').innerHTML = "En tant que Chargé de projet Digital ou Product Owner";
+  }
+
+  if (count == 9) {
+    document.getElementById('text').innerHTML = "J'ai des compétences variés";
+  }
+
+  if (count == 10) {
+    document.getElementById('text').innerHTML = "Dévellopement web";
+  }
+
+  if (count == 11) {
+    document.getElementById('text').innerHTML = "Design";
+  }
+
+  if (count == 12) {
+    document.getElementById('text').innerHTML = "Communication";
+  }
+
+  if (count == 13) {
+    document.getElementById('text').innerHTML = "Webmarketing";
+  }
+
+  if (count == 14) {
+    document.getElementById('text').innerHTML = "Montage Vidéo";
+  }
+
+  if (count == 15) {
+    document.getElementById('text').innerHTML = "Et beaucoup d'autres choses";
+  }
+
+  if (count == 16) {
+    document.getElementById('text').innerHTML = "J'ai un parcourt déjà bien rempli";
+  }
+
+  if (count == 17) {
+    document.getElementById('text').innerHTML = "Bac ES avec mention";
+  }
+
+  if (count == 18) {
+    document.getElementById('text').innerHTML = "Un ans chez DiX une école qui m'a fait découvrir le web et faire plein de projets";
+  }
+
+  if (count == 19) {
+    document.getElementById('text').innerHTML = "Puis une seconde année dans le Digital à l'ESD Paris";
+  }
+
+  if (count == 20) {
+    document.getElementById('text').innerHTML = "Ou j'ai fait des projets de plus grosse envergure";
+  }
+
+  if (count == 21) {
+    document.getElementById('text').innerHTML = "Comme la céation d'un restaurant";
+  }
+
+  if (count == 22) {
+    document.getElementById('text').innerHTML = "Un montage vidéo pour la soirée de l'école";
+  }
+
+  if (count == 23) {
+    document.getElementById('text').innerHTML = "Ou un projet en collaboration avec Adidas et Neurun";
+  }
+
+  if (count == 24) {
+    document.getElementById('text').innerHTML = "Mais c'est pas tout";
+  }
+
+  if (count == 25) {
+    document.getElementById('text').innerHTML = "J'ai aussi fait un stage dans une start-up Share.Place";
+  }
+
+  if (count == 26) {
+    document.getElementById('text').innerHTML = "Pendant 5 mois";
+  }
+
+  if (count == 27) {
+    document.getElementById('text').innerHTML = "J'ai nottament fait du marketing et de la gestion de produit.";
+  }
+
+  if (count == 28) {
+    document.getElementById('text').innerHTML = "Voilà.";
+  }
+
+  if (count == 29) {
+    document.getElementById('text').innerHTML = "Si vous voulez en savoir plus il faudra gagner à un petit jeu";
+  }
+
+  if (count == 30) {
+    document.getElementById('text').innerHTML = "Car je suis très joueur";
+  }
+
+  if (count == 31) {
+    document.getElementById('btn').innerHTML = "Jouer !";
+    document.getElementById('text').innerHTML = "Bonne Chance !";
+  }
+
+  if (count == 32) {
+    document.getElementById('btn').innerHTML = "Cliquez !";
+    document.getElementById('text').innerHTML = "C'est très facile";
+  }
+
+  if (count == 33) {
+    document.getElementById('text').innerHTML = "Il faut cliquer une toute dernière fois";
+  }
+
+  if (count == 34) {
+    document.getElementById('btn').classList.add("none")
+    document.getElementById('text').innerHTML = "Voilà vous avez réussi ! <br> Merci d'avoir pris de votre temps pour moi !  <br><br> Vous pouvez me contacter ici : <br>Email : simon.destanque@gmail.com <br> Tel : 06.81.37.89.83";
+  }
+});
